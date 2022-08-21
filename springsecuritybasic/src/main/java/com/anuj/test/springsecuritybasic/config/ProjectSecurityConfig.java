a1b80fb73eaca199f645cb131141dee9f62e26b1
reversosticker <anujpathak9335@gmail.com>
2022-08-21 22:13:36 +0530

    security using mysql database

--- springsecuritybasic/src/main/java/com/anuj/test/springsecuritybasic/config/ProjectSecurityConfig.java
+++ springsecuritybasic/src/main/java/com/anuj/test/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -6,10 +6,14 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -38,6 +42,11 @@ public class ProjectSecurityConfig {
 //                .httpBasic(Customizer.withDefaults());
 //
         return (SecurityFilterChain) http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
