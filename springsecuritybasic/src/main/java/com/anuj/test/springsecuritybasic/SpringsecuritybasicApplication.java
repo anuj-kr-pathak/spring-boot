e59a56798005d17842056a5c4bb690ed941a0419
reversosticker <anujpathak9335@gmail.com>
2023-10-25 02:03:53 +0530

    view all default security filter present in spring :: FilterChainProxy

--- springsecuritybasic/src/main/java/com/anuj/test/springsecuritybasic/SpringsecuritybasicApplication.java
+++ springsecuritybasic/src/main/java/com/anuj/test/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -2,8 +2,10 @@ package com.anuj.test.springsecuritybasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class SpringsecuritybasicApplication {
 
 	public static void main(String[] args) {
